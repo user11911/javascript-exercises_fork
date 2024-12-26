c3730ebba7854e388459007416be134e4ccf3c3f
Alberto Rigoni <albertorigoni200@gmail.com>
2024-12-26 20:04:48 +0100

    end result

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+
+    const eliminations = args.slice(1);
+
+    let to_return = array;
+
+    for (let member of to_return){
+        for (let elimination of eliminations){
+            if (member === elimination){
+                array.splice(to_return.indexOf(member), 1);
+            }
+        }
+    }
+    
+    return to_return;
 };
 
-console.log("hello word");
+// takes an array and after, all the things which user wants to delete from that array. returns the array less all the things which were wanted to be deleted. does so trough a for of loop in  the array and a for of loop for the required eliminations. returns a new modified array.
+
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
